398bee330b30e67b3885cdc45cff05bae91e94ab
Chao Tzu Hsuan <410208044@gms.ndhu.edu.tw>
2024-10-02 03:31:54 +0800

    新增 酒譜搜尋頁第三區塊 swiper

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./assets/js/swiper";
 console.log("Hello world!");
