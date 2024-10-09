130ac73fae23251c8cba9dda07804edd1e20790a
Chen <s99214033@gmail.com>
2024-10-09 13:47:23 +0800

    更新main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./assets/js/swiper";
+import "./assets/js/index";
+import "./assets/js/barsearch";
+
 console.log("Hello world!");
