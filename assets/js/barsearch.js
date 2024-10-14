d121a650c92642ec6015c8d4e87a40f6716ee182
Chen <s99214033@gmail.com>
2024-10-14 14:03:40 +0800

    抽換首頁圖片及文案、改首頁slide寬度

--- assets/js/barsearch.js
+++ assets/js/barsearch.js
@@ -85,6 +85,23 @@ let popularBarsSwiper = new Swiper(".popularBarsSwiper", {
   },
 });
 
+let mySwiper1013 = new Swiper(".mySwiper1013", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    992: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+});
 // 周邊酒吧活動
 // const swiper = new Swiper(".mySwipe", {
 //   loop: true,
