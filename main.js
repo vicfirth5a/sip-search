179bb4ec5ffa83e0a4b850945ec465f0426bfcb8
Chao Tzu Hsuan <410208044@gms.ndhu.edu.tw>
2024-10-15 15:48:39 +0800

    修改 成功導入jquery 基酒區塊左右滑動成功

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./assets/js/swiper";
-import "./assets/js/jquery-3.7.1";
 import "./assets/js/index";
 import "./assets/js/barsearch";
+import "./assets/js/recipessearch";
 
 console.log("Hello world!");
