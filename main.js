a6c528e6d03fb64bfb9ce74ad80346f250fb7312
Chao Tzu Hsuan <410208044@gms.ndhu.edu.tw>
2024-10-16 15:40:55 +0800

    修改 導入jQuery CDN，在main.js引入酒譜搜尋頁js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./assets/js/swiper";
-import "./assets/js/jquery-3.7.1.js";
 import "./assets/js/index";
-import "./assets/js/barsearch";
 import "./assets/js/recipessearch";
+import "./assets/js/barsearch";
 
 console.log("Hello world!");
