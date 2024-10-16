bfc412d923b330cb4dbed5a0c5d3e84bbfa563af
Chen <s99214033@gmail.com>
2024-10-16 15:51:52 +0800

    test 酒譜jquery

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "./assets/js/swiper";
 import "./assets/js/index";
 import "./assets/js/barsearch";
+import "./assets/js/jquery-3.7.1";
 import "./assets/js/recipessearch";
 
 console.log("Hello world!");
