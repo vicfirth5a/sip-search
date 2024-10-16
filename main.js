11e6fe335b0818adee2d2215f617cfab2df6b793
Chao Tzu Hsuan <410208044@gms.ndhu.edu.tw>
2024-10-16 15:01:03 +0800

    把jquery引入main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./assets/js/swiper";
+import "./assets/js/jquery-3.7.1.js";
 import "./assets/js/index";
 import "./assets/js/barsearch";
 import "./assets/js/recipessearch";
