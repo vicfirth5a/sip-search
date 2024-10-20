ea0ae327902b55c514ddbd19dff57177cbd1d83e
Chao Tzu Hsuan <410208044@gms.ndhu.edu.tw>
2024-10-20 16:42:12 +0800

    修改 還是使用CDN的方式

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
-import "./node_modules/jquery/dist/jquery";
+// import "./node_modules/jquery/dist/jquery";
 import "./assets/js/about";
 import "./assets/js/index";
 import "./assets/js/recipessearch";
